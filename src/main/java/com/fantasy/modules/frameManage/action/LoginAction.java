cb2225d8773a715493478a20bfdbb55b67562a44
silence <silence@bibo-pc>
2016-05-13 00:42:21 +0800

    开发中

--- src/main/java/com/fantasy/modules/frameManage/action/LoginAction.java
+++ src/main/java/com/fantasy/modules/frameManage/action/LoginAction.java
@@ -1,5 +1,10 @@
 package com.fantasy.modules.frameManage.action;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,39 +16,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fantasy.modules.userManage.model.User;
+
 
 @Controller
-@Scope("prototype")
+//@Scope("prototype")
 @RequestMapping("/login")
 public class LoginAction {
 	
 	@RequestMapping(value = "/check")
 	public ModelAndView check() {
-		System.out.println("查询");
 
 		ModelAndView mov=new ModelAndView();  
         mov.setViewName("login");
-        //mov.addObject("msg", "(用户"+name+" delete success)");
         
 		return mov;
 	}
 	
 	@RequestMapping(value = "/query")
-	public ModelAndView query(HttpServletRequest request,HttpServletResponse response) {
-		//解码  
+	public String query(HttpServletRequest request,HttpServletResponse response) {
+//		System.out.println("chaxun");
+//		Map<String, String[]> paramMap = request.getParameterMap();
+//		System.out.println(paramMap.get("orderJson"));
+//		
+//		
 //        String str = request.getParameter("orderJson");
 //        System.out.println(str);
 //        JSONObject jb=new JSONObject();   
-//        //将json格式的字符串转换为json对象，并取得该对象的“userName”属性值  
 //        String o=(String)jb.fromObject(str).get("username");  
 //        System.out.println(o);
-        
-        
-
+		
+        //System.out.println(user.getUsername());
+		System.out.println("鏌ヨ");
 		ModelAndView mov=new ModelAndView();  
-        mov.setViewName("modules/main");
-        //mov.addObject("msg", "(用户"+name+" delete success)");
+		mov.setViewName("32145");
         
-		return mov;
+        User user2=new User();
+        user2.setUsername("tom");
+        user2.setPassword("qwe");
+        mov.addObject(user2);
+        
+        
+        List<User> list = new ArrayList<User>();
+        User um = new User();
+        um.setUsername("sss");
+        um.setPassword("123");
+        list.add(um);
+        Map<String, Object> modelMap = new HashMap<String, Object>(3);
+        modelMap.put("total", "1");
+        modelMap.put("data", list);
+        modelMap.put("success", "true");
+		return null;
 	}
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,6 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -12,39 +11,52 @@
 <script type="text/javascript">
 	$(function() {
 		
-		$('form')
 		
-		$('form').submit(function() {
+		
+		/* $('#save').click(function(){
+			$('form').attr({
+				action:"${pageContext.request.contextPath}/login/query",
+				method:"post"
+			});
+			
+			$('form').submit();
+		}); */
+		
+		
+		//$('#save').submit("",function(){});
+		
+		$('#save').click(function(){
 			var username = $("#username").val();
 			var password = $("#password").val();
-
+			
 			var user = {
 				username : username,
 				password : password
 			};
-			var aMenu = JSON.stringify(user);
-
+			var userJson = JSON.stringify(user);
 			$.ajax({
-				type : "post",
-				url : "login/query",
-				data : "orderJson=" + aMenu
+				type:"post",
+				dataType : 'json',
+                url:"${pageContext.request.contextPath}/login/query",  
+                data:user,
+                success:function(response,status,xhr){  
+                    alert(response);  
+                },
+                error:function(e) {  
+                    alert("出错："+e);  
+                }  
 			});
 		});
-		
 	});
+	
 </script>
 </head>
-
-
 <body>
-	<form>
-		用户名:<input type="text" id="username"></input> 
-		密码:<input type="text" id="password"></input>
-		<button type="button" id="save">提交</button>
-		<input type="submit" value="测试">
-	</form>
-	<form action="${pageContext.request.contextPath}/login/query" method="post">
-		<input type="submit" value="测试1">
-	</form>
+	
+		用户名:<input type="text" name="username" id="username"></input> 
+		密码:<input type="text" name="password" id="password"></input>
+		<input type="button" value="测试" id="save" >
+	
+
 </body>
 </html>
