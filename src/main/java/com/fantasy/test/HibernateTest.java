e6f66b2a17e0b0fcbfd11e299147cbb668355ce4
silence <silence@bibo-pc>
2016-05-31 22:02:26 +0800

    hibernate测试

--- src/main/java/com/fantasy/test/HibernateTest.java
+++ src/main/java/com/fantasy/test/HibernateTest.java
@@ -1,15 +1,29 @@
 package com.fantasy.test;
 
 import org.hibernate.SessionFactory;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fantasy.modules.functionManage.service.impl.SysFunctionService;
 
 public class HibernateTest {
 	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	public void test(){
 		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
 		SessionFactory session=(SessionFactory) factory.getBean("sessionFactory");
-		session.openSession();
+		session.getCurrentSession();
+	}
+	@Test
+	@Transactional
+	public void test1(){
+		test();
+		
 	}
 }
